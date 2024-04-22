8db5dc92deafdbe2cd72378986f6f090fcec95b6
Kris-Chil <Kristinemchilson@gmail.com>
2024-04-22 15:08:17 +0200

    puzzle manager

---
+++ Assets/Scripts/Managers/PuzzleManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PuzzleManager : MonoBehaviour, IDataPersistence
+{
+    [Header("Task Id")] 
+    [SerializeField] private string taskId;
+    private bool taskDone;
+
+    private void Start()
+    {
+        taskDone = false;
+    }
+
+    public void PuzzleOver()
+    {
+        taskDone = true;
+        TaskManager.GetInstance().TaskComplete(taskId);
+        DataPersistenceManager.instance.SaveGame();
+    }
+    
+    public void LoadData(GameData data)
+    {
+        data.tasksList.TryGetValue(taskId, out taskDone);
+    }
+
+    public void SaveData(GameData data)
+    {
+        if (data.tasksList.ContainsKey(taskId))
+        {
+            data.tasksList.Remove(taskId);
+        }
+
+        data.tasksList.Add(taskId, taskDone);
+    }
+
+    public void SaveTaskData(GameData data) { }
+    
+    public void LoadTaskData(GameData data) { }
+    
+    public void LoadKeyData(GameData data){}
+    public void SaveKeyData(GameData data){}
+}
