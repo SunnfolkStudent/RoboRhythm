a3c309542c7651be6b998756e219202327b57d5a
Tusda <marcus.wiborg@outlook.com>
2024-04-25 20:02:03 +0200

    PauseEnablerClass

--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -2,6 +2,10 @@ using UnityEngine.SceneManagement;
 
 public static class GameEvents
 {
+    public delegate void GameEvent();
+    public static GameEvent gamePaused;
+    public static GameEvent gameUnpaused;
+    
     public delegate void LoadingEvent(string sceneName);
     public static LoadingEvent unloadScene;
 }
