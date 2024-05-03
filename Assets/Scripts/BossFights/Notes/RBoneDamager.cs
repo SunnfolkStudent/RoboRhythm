0304382f28fb6376625a5a04eac64544595d1b91
99Caleb <calebmchilson@gmail.com>
2024-05-03 15:19:25 +0200

    Clock Boss Update 2

---
+++ Assets/Scripts/BossFights/Notes/RBoneDamager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RBoneDamager : MonoBehaviour
+{
+    [SerializeField] private int dealDamage;
+    [SerializeField] private float destroyAfterSeconds;
+    
+    private RHealthManager _healthManager;
+    void Start()
+    {
+        _healthManager = FindObjectOfType<RHealthManager>();
+        Destroy(gameObject,destroyAfterSeconds);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "PlayerHitBox")
+        {
+            Debug.Log("Hit");
+            _healthManager.TakeDamage(dealDamage);
+        }
+    }
+}
